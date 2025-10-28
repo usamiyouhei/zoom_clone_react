6ca398d569c383093c516fd3a27ce15066fb0065
youhei usami <usao9110@icloud.com>
2025-10-28 23:57:46 +0400

    10/28

--- src/modules/meetings/meeting.repository.ts
+++ src/modules/meetings/meeting.repository.ts
@@ -5,5 +5,9 @@ export const meetingRepository = {
   async createMeeting(): Promise<{ meetingId : string }> {
     const result = await api.post('/meetings');
     return result.data;
-  }
+  },
+  async joinMeeting(meetingId: string): Promise<{ meetingId: string}> {
+    const result = await api.post(`/meetings/join/${meetingId}`);
+    return result.data;
+  },
 }
