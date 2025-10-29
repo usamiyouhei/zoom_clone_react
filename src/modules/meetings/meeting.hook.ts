4823f925917bba90fc5210ae7c3f22b854c008d9
youhei usami <usao9110@icloud.com>
2025-10-29 08:53:44 +0400

    10/28

--- src/modules/meetings/meeting.hook.ts
+++ src/modules/meetings/meeting.hook.ts
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { useAtomValue } from "jotai";
+import { useEffect, useState } from "react";
+import { currentUserAtom } from "../auth/current-user.state";
+
+export interface Participant {
+  id: string;
+  name: string;
+  stream: MediaStream | null;
+}
 
 export const useMeeting = () => {
-
   const [localStreams, setLocalStreams] = useState<MediaStream[]>([]);
+  const currentUser = useAtomValue(currentUserAtom);
+  const [me, setMe] = useState<Participant>({
+    id: currentUser!.id,
+    name:currentUser!.name,
+    stream: localStreams[0]
+  })
+
+  useEffect(() => {
+    setMe((prev) => ({ ...prev, stream: localStreams[0]}))
+  }, [localStreams])
+  
 
   const getStream = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -10,5 +28,7 @@ export const useMeeting = () => {
       audio: true,
     });
     setLocalStreams((prev) => [...prev, stream])
-  }
+  };
+
+  return { me , getStream }
 }
